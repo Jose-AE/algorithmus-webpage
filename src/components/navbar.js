c18959ef73fffdcf15c66153b08032e0d544f4fc
Gatuno7000 <107892696+Jose-AE@users.noreply.github.com>
2023-02-23 00:54:43 -0600

    compact nav bar fix

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -12,10 +12,10 @@ export default function Navbar(){
             <img className="logo" src={require("../assets/images/Logo.png")} alt="Logo" width="100" height="100"/>
 
             <div className={ `${isOpen?"nav-items open":"nav-items"}`  }>
-                <Link to="/">Inicio</Link>
-                <Link to="projects">Proyectos</Link>
-                <Link to="members">Integrantes</Link>
-                <Link to="contact">Contacto</Link> 
+                <Link to="/" onClick={() => setIsOpen(false)}>Inicio</Link>
+                <Link to="projects" onClick={() => setIsOpen(false)}>Proyectos</Link>
+                <Link to="members" onClick={() => setIsOpen(false)}>Integrantes</Link>
+                <Link to="contact" onClick={() => setIsOpen(false)}>Contacto</Link> 
             </div>
 
             <div className={ `${isOpen?"nav-toggle open":"nav-toggle"}`  } onClick={() => setIsOpen(!isOpen)}>
